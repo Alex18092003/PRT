5e5f1b2a2eab8b0f97be0430fb25678aee3cdfc2
AleksandraLebedeva <sashyla1809@gmail.com>
2022-11-12 03:32:58 +0300

    сортировка

--- app/src/main/java/com/example/prt/conclusion.java
+++ app/src/main/java/com/example/prt/conclusion.java
@@ -1,14 +1,19 @@
 package com.example.prt;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -19,53 +24,96 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class conclusion extends AppCompatActivity {
 
-
+    private Spinner spinner;
     private AdapterMask pAdapter;
+    ListView lvProduct;
     private List<DataModal> listProduct = new ArrayList<>();
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conclusion);
 
+        lvProduct = findViewById(R.id.ListProduct);
         ListView ivProducts = findViewById(R.id.ListProduct);
         pAdapter = new AdapterMask(conclusion.this, listProduct);
         ivProducts.setAdapter(pAdapter);
 
+        List<String> list = new ArrayList<String>();
+        list.add("Без сортировки");
+        list.add("Сортировка по именам");
+        list.add("Сортировка по фамилии");
+
+        spinner = findViewById(R.id.Spinner);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+            SortNameSurname(listProduct);
+        }
+        @Override
+        public void onNothingSelected(AdapterView<?> parentView) {
+        }
+
+    });
         new GetTeacher().execute();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void SortNameSurname(List<DataModal> list){
+        lvProduct.setAdapter(null);
+        switch(spinner.getSelectedItemPosition()){
+            case 0:
+                new GetTeacher().execute();
+                break;
+            case 1:
+                Collections.sort(list, Comparator.comparing(DataModal::getName));
+                break;
+            case 2:
+                Collections.sort(list, Comparator.comparing(DataModal::getSurname));
+                break;
+            default:
+                break;
+        }
+        pAdapter = new AdapterMask(conclusion.this,list);
+        lvProduct.setAdapter(pAdapter);
+        pAdapter.notifyDataSetInvalidated();
+    }
+
+    // вывод данных
     private class GetTeacher extends AsyncTask<Void, Void, String> {
 
         @Override
         protected String doInBackground(Void... voids) {
             try {
-
                 URL url = new URL("https://ngknn.ru:5001/NGKNN/лебедевааф/api/Teachers");
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
                 BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 StringBuilder result = new StringBuilder();
                 String line = "";
-
                 while ((line = reader.readLine()) != null) {
                     result.append(line);
                 }
                 return result.toString();
-
             } catch (Exception exception) {
                 return null;
             }
         }
-
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             try
             {
+                listProduct.clear();
                 JSONArray tempArray = new JSONArray(s);
                 for (int i = 0;i<tempArray.length();i++)
                 {
@@ -86,8 +134,6 @@ public class conclusion extends AppCompatActivity {
                 Toast.makeText(conclusion.this, "Что-то пошло не так", Toast.LENGTH_LONG).show();
             }
         }
-
-
     }
 
     public  void  goAdd(View view) // переход в окно ввода информации
